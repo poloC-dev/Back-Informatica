6e1c01b93fa6fbbc3996263836300b17a502e574
poloC-dev <polochariatti@gmail.com>
2023-04-17 15:33:11 -0300

    cors in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require("./src/server/server");
 require("./src/database/db");
 require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors({ origin: true, credentials: true }));
 
 const PORT = process.env.PORT || 8080;
 
